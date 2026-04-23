7b8f74afa13bf715347eee468eaabca247fa626c
6ix7car <mihanavi04@mail.ru>
2026-04-23 21:53:54 +1000

    feat: добавил класс UpdateService

---
+++ UpdateService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kurs
+{
+    public static class UpdateService
+    {
+        public static bool CheckForUpdates()
+        {
+            Console.WriteLine("Проверка обновлений...");
+            return false;
+        }
+
+        public static void ApplyUpdate()
+        {
+            Console.WriteLine("Применение обновления...");
+            SecurityLogger.Log("UPDATE", "system", "127.0.0.1", "Обновление установлено", "INFO");
+        }
+    }
+}
