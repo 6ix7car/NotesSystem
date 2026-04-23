7bb818493aa76af26f9ebfb0abff40d38157c6d4
6ix7car <mihanavi04@mail.ru>
2026-04-23 21:49:29 +1000

    feat: добавил класс SecurityLoggerService

---
+++ SecurityLogger.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+
+namespace Kurs
+{
+    public static class SecurityLogger
+    {
+        public static void Log(string eventType, string username, string ip, string description, string severity = "INFO")
+        {
+            string query = @"INSERT INTO security_logs (event_type, username, user_ip, description, severity, created_at) 
+                             VALUES (@et, @u, @ip, @desc, @sev, @now)";
+            var parameters = new[]
+            {
+                new NpgsqlParameter("et", eventType),
+                new NpgsqlParameter("u", username),
+                new NpgsqlParameter("ip", ip),
+                new NpgsqlParameter("desc", description),
+                new NpgsqlParameter("sev", severity),
+                new NpgsqlParameter("now", System.DateTime.Now)
+            };
+            DbHelper.ExecuteNonQuery(query, parameters);
+        }
+    }
+}
